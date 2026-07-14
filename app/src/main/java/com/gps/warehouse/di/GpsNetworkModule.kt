aba99d586317434a84bb54256d692ee3707c7e2d
Timur Malyshev <timur.malyshev@hmmr.ru>
2026-07-14 14:15:46 +0300

    Полностью удалил активы

--- app/src/main/java/com/gps/warehouse/di/GpsNetworkModule.kt
+++ app/src/main/java/com/gps/warehouse/di/GpsNetworkModule.kt
@@ -23,7 +23,7 @@ import javax.inject.Singleton
  */
 @Module
 @InstallIn(SingletonComponent::class)
-object NetworkModule {
+object GpsNetworkModule {
 
     /**
      * Предоставляет экземпляр OkHttpClient.
