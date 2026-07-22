32bccaa47399dad74f8a36eaef74f81521577f71
Timur Malyshev <timur.malyshev@hmmr.ru>
2026-07-22 16:37:04 +0300

    add android data

--- app/src/main/java/com/gps/warehouse/ui/assets_screens/MobileDeviceDetailScreen.kt
+++ app/src/main/java/com/gps/warehouse/ui/assets_screens/MobileDeviceDetailScreen.kt
@@ -11,8 +11,17 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.BatteryChargingFull
+import androidx.compose.material.icons.filled.BatteryFull
+import androidx.compose.material.icons.filled.BatterySaver
+import androidx.compose.material.icons.filled.Handyman
+import androidx.compose.material.icons.filled.PhoneAndroid
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.WifiTethering
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -23,6 +32,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -51,6 +61,7 @@ fun MobileDeviceDetailScreen(
         is UiState.Loading -> {
             CustomLoadingView()
         }
+
         is UiState.Error -> {
             ErrorStateView(
                 message = state.message,
@@ -58,6 +69,7 @@ fun MobileDeviceDetailScreen(
                 modifier = Modifier.fillMaxSize()
             )
         }
+
         is UiState.Success -> {
             val device = state.devices.firstOrNull()
             if (device != null) {
@@ -88,48 +100,70 @@ fun MobileDeviceDetailContent(
         }
     ) { paddingValues ->
         // Используем ваш кастомный скролл
-        Column (
+        Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues)
                 .padding(16.dp)
                 .verticalScroll(rememberScrollState()),
         ) {
-            InfoSection(title = "📱 Устройство") {
-                InfoRow("Модель", device.device?.model)
-                InfoRow("Имя", device.device?.name)
-                InfoRow("Серийный номер", device.serial_number)
-                InfoRow("Был в сети", device.request_time)
-            }
+            InfoSection(
+                title = "Устройство",
+                icon = Icons.Default.PhoneAndroid,
+                {
+                    InfoRow("Модель", device.device?.model)
+                    InfoRow("Имя", device.device?.name)
+                    InfoRow("Серийный номер", device.serial_number)
+                    InfoRow("Был в сети", device.request_time)
+                },
+            )
 
-            InfoSection(title = "⚙️ Система") {
-                InfoRow("Версия Android", device.system?.android_version)
-                InfoRow("API Level", device.system?.android_api_version)
-                InfoRow("Сборка", device.system?.build_number)
-                InfoRow("Язык", device.system?.language)
-                InfoRow("Часовой пояс", device.system?.timezone)
-                InfoRow("Аптайм", device.system?.uptime)
-            }
+            InfoSection(
+                title = "Система",
+                icon = Icons.Default.Settings,
+                {
+                    InfoRow("Версия Android", device.system?.android_version)
+                    InfoRow("API Level", device.system?.android_api_version)
+                    InfoRow("Сборка", device.system?.build_number)
+                    InfoRow("Язык", device.system?.language)
+                    InfoRow("Часовой пояс", device.system?.timezone)
+                    InfoRow("Аптайм", device.system?.uptime)
+                },
+            )
 
-            InfoSection(title = "🛠️ Железо") {
-                InfoRow("Процессор", device.hardware?.processor)
-                InfoRow("Архитектура", device.hardware?.processor_architecture)
-                InfoRow("ОЗУ (Всего / Свободно)", "${device.hardware?.ram_total} / ${device.hardware?.ram_free}")
-                InfoRow("Память (Всего / Свободно)", "${device.hardware?.storage_total} / ${device.hardware?.storage_free}")
-                InfoRow("Камеры", device.hardware?.cameras)
-                InfoRow("Разрешение экрана", device.hardware?.screen_resolution)
-            }
+            InfoSection(
+                title = "Железо",
+                icon = Icons.Default.Handyman,
+                {
+                    InfoRow("Процессор", device.hardware?.processor)
+                    InfoRow("Архитектура", device.hardware?.processor_architecture)
+                    InfoRow(
+                        "ОЗУ (Всего / Свободно)",
+                        "${device.hardware?.ram_total} / ${device.hardware?.ram_free}"
+                    )
+                    InfoRow(
+                        "Память (Всего / Свободно)",
+                        "${device.hardware?.storage_total} / ${device.hardware?.storage_free}"
+                    )
+                    InfoRow("Камеры", device.hardware?.cameras)
+                    InfoRow("Разрешение экрана", device.hardware?.screen_resolution)
+                },
+            )
 
-            InfoSection(title = "🌐 Сеть") {
-                InfoRow("Тип подключения", device.network?.connection_type)
-                InfoRow("Wi-Fi SSID", device.network?.wifi_ssid ?: "Скрыт/Нет")
-                InfoRow("Шлюз", device.network?.wifi_gateway)
-                InfoRow("MAC-адрес", device.network?.mac_address)
-                InfoRow("IP-адреса", device.network?.ip_addresses)
-                InfoRow("Bluetooth", device.network?.bluetooth)
-            }
+            InfoSection(
+                title = "Сеть",
+                icon = Icons.Default.WifiTethering,
+                {
+                    InfoRow("Тип подключения", device.network?.connection_type)
+                    InfoRow("Wi-Fi SSID", device.network?.wifi_ssid ?: "Скрыт/Нет")
+                    InfoRow("Шлюз", device.network?.wifi_gateway)
+                    InfoRow("MAC-адрес", device.network?.mac_address)
+                    InfoRow("IP-адреса", device.network?.ip_addresses)
+                    InfoRow("Bluetooth", device.network?.bluetooth)
+                },
+            )
 
-            InfoSection(title = "🔋 Батарея") {
+            InfoSection(title = "Батарея", icon = Icons.Default.BatteryChargingFull) {
                 InfoRow("Уровень", device.battery?.level)
                 InfoRow("Статус", device.battery?.status)
                 InfoRow("Температура", device.battery?.temperature)
@@ -139,10 +173,20 @@ fun MobileDeviceDetailContent(
 }
 
 @Composable
-fun InfoSection(title: String, content: @Composable ColumnScope.() -> Unit) {
-    Card(modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp)) {
+fun InfoSection(title: String, icon: ImageVector, content: @Composable ColumnScope.() -> Unit) {
+    Card(modifier = Modifier
+        .fillMaxWidth()
+        .padding(vertical = 8.dp)) {
         Column(modifier = Modifier.padding(16.dp)) {
-            Text(text = title, style = MaterialTheme.typography.titleMedium, color = MaterialTheme.colorScheme.primary)
+            Row(modifier = Modifier) {
+                Icon(imageVector = icon, contentDescription = title)
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.primary
+                )
+
+            }
             Spacer(modifier = Modifier.height(8.dp))
             content()
         }
@@ -151,15 +195,24 @@ fun InfoSection(title: String, content: @Composable ColumnScope.() -> Unit) {
 
 @Composable
 fun InfoRow(label: String, value: String?) {
-    Row(modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp), horizontalArrangement = Arrangement.SpaceBetween) {
-        Text(text = label, style = MaterialTheme.typography.bodyMedium, color = MaterialTheme.colorScheme.onSurfaceVariant)
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = label,
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.onSurfaceVariant
+        )
         Text(text = value ?: "N/A", style = MaterialTheme.typography.bodyMedium)
     }
 }
 
 
 // PREVIEW
-@Preview(showBackground = true, name = "Детали устройства", device = Devices.PIXEL_4)
+@Preview(showBackground = true, name = "Детали устройства", device = "spec:width=380dp,height=1200dp")
 @Composable
 private fun MobileDeviceDetailContentPreview() {
     MaterialTheme(colorScheme = lightColorScheme()) {
@@ -172,7 +225,7 @@ private fun MobileDeviceDetailContentPreview() {
     }
 }
 
-@Preview(showBackground = true, name = "Детали устройства (не найдено)", device = Devices.PIXEL_4)
+@Preview(showBackground = true, name = "Детали устройства (не найдено)", device = "spec:width=380dp,height=1200dp")
 @Composable
 private fun MobileDeviceDetailContentNotFoundPreview() {
     MaterialTheme(colorScheme = lightColorScheme()) {
