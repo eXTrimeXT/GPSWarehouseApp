3357bd4723445266226d4b1a18d6d0ebdbf82313
Timur Malyshev <timur.malyshev@hmmr.ru>
2026-08-10 12:31:52 +0300

    Подготовка к PRODUCTION

--- app/src/main/java/com/gps/warehouse/utils/Constants.kt
+++ app/src/main/java/com/gps/warehouse/utils/Constants.kt
@@ -1,11 +1,14 @@
 package com.gps.warehouse.utils
 
 object Constants {
+    // TEST
     const val BASE_URL_API = "http://gps-test.hmmr.ru/api/"
-//    const val BASE_URL_API = "https://gps-rs.hmmr.ru/api/"
-
     const val BASE_URL_UPDATE = "http://10.168.143.7:8100/test"
-//    const val BASE_URL_UPDATE = "http://10.168.143.7:8100/prod"
+
+    // PROD
+    // const val BASE_URL_API = "https://gps-rs.hmmr.ru/api/"
+    // const val BASE_URL_UPDATE = "http://10.168.143.7:8100/prod"
+
     // https://git-new.hmmr.ru/timurmalyshev/android-gps-warehouse-app/-/jobs/1086/artifacts/download?file_type=archive
     // https://git-new.hmmr.ru/timurmalyshev/android-gps-warehouse-app/-/jobs/1077/artifacts/download?file_type=archive
     const val TOKEN_PREFS_NAME = "gps_token_prefs"
